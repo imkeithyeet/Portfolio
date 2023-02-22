161fe595e4dd9a8ea11c64d262625574bf973ed9
Keith Ma <95742118+imkeithyeet@users.noreply.github.com>
2023-02-21 20:46:28 -0800

    bar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,19 +1,48 @@
 import React from 'react';
 import Image from 'next/image'
 import Link from 'next/link';
-
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
 const Navbar = () => {
     return (
         <div className="fixed w-full h-20 shadow-xl z-[100]">
             <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
             <Image src="/../public/assets/navLogo.png" alt="/" width="229" height="50"/>
             <div>
-                <ul>
+                <ul className='hidden md:flex'>
                     <Link href="/">
                         <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
                      </Link>
+                     <Link href="/">
+                        <li className="ml-10 text-sm uppercase hover:border-b">About</li>
+                     </Link>
+                     <Link href="/">
+                        <li className="ml-10 text-sm uppercase hover:border-b">Skills</li>
+                     </Link>
+                     <Link href="/">
+                        <li className="ml-10 text-sm uppercase hover:border-b">Projects</li>
+                     </Link>
+                     <Link href="/">
+                        <li className="ml-10 text-sm uppercase hover:border-b">Contact</li>
+                     </Link>
                 </ul>
+                <div className='md:hidden'>
+                    <AiOutlineMenu size={25}/>
+                </div>
             </div>
+            </div>
+            <div className='fixed left-0 top-0 w-full h-screen bg-black/70'>
+                <div className='fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'>
+                    <div>
+                        <div>
+                            <Image src='/../public/assets/navLogo.png' width="250" height="100" alt='/'/>
+                            <div>
+                                
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
             </div>
         </div>
     );

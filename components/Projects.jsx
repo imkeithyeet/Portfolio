de6fc76b1f751a752a8be43c985ff827c5740c77
Keith Ma <95742118+imkeithyeet@users.noreply.github.com>
2023-02-22 22:52:06 -0800

    projects

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,4 +1,7 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
+import usDatingImg from '../public/assets/projects/usDating.jpg'
 
 const Projects = () => {
     return (
@@ -8,10 +11,17 @@ const Projects = () => {
                 <h2 className="py-4">What I&apos;ve Built</h2>
                 <div className="grid md:grid-cols-2 gap-8">
                     <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#636393] to-[#5272b8]">
-
+                    <Image className='rounded-xl group-hover:opacity-10' src={usDatingImg} width="700" alt="/"/>
+                    <div className="hidden group-hover:block">
+                        <h3>Us Dating App Website</h3>
+                        <p>React JS With Ruby on Rails</p>
+                        <Link href='/'>
+                            <p>More Info</p>
+                        </Link>
+                    </div>
                     </div>
                 </div>
-            </div>
+            </div> 
         </div>
     );
 }

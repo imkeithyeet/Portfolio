877402bb04b838fcad3b18e2581cc32b8a7fa378
Keith Ma <95742118+imkeithyeet@users.noreply.github.com>
2023-02-22 19:54:50 -0800

    skills

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 
 const Skills = () => {
     return (
-        <div>
+        <div className="w-full lg:h-screen p-2">
+            <div className="max-w-[1240px] mex-auto flex flex-col justify-center h-full">
+            <p className='text-xl tracking-widest uppercase text-[#4441a3]'>Skills</p>
+            <h2 className='py-4'>What I Can Do</h2>
+            <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+
+            </div>
+            </div>
             
         </div>
     );

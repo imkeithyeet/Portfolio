dd46ba8268df52d1306e26d8ad20ee1c1ddd629d
Keith Ma <95742118+imkeithyeet@users.noreply.github.com>
2023-02-22 23:21:40 -0800

    projects done

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -2,6 +2,9 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import usDatingImg from '../public/assets/projects/usDating.jpg'
+import BirdviewImg from '../public/assets/projects/Birdview.jpg'
+
+import ProjectItem from './ProjectItem';
 
 const Projects = () => {
     return (
@@ -10,16 +13,16 @@ const Projects = () => {
                 <p className="text-xl tracking-widest uppercase text-[#4441a3]">Projects</p>
                 <h2 className="py-4">What I&apos;ve Built</h2>
                 <div className="grid md:grid-cols-2 gap-8">
-                    <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#636393] to-[#5272b8]">
-                    <Image className='rounded-xl group-hover:opacity-10' src={usDatingImg} width="700" alt="/"/>
-                    <div className="hidden group-hover:block">
-                        <h3>Us Dating App Website</h3>
-                        <p>React JS With Ruby on Rails</p>
-                        <Link href='/'>
-                            <p>More Info</p>
-                        </Link>
-                    </div>
-                    </div>
+                  <ProjectItem 
+                  title='Us Dating App Website'  
+                  backgroundImg={usDatingImg} 
+                  projectUrl="/Us"
+                  />
+                   <ProjectItem 
+                  title='BirdView House Listing'  
+                  backgroundImg={BirdviewImg}
+                  projectUrl="/Us"
+                  />
                 </div>
             </div> 
         </div>

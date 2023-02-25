0e0ee5f87a6c277585d3d30beef29f3bc5c54a83
Keith Ma <95742118+imkeithyeet@users.noreply.github.com>
2023-02-25 14:43:58 +0800

    scroll up

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -4,6 +4,8 @@ import ContactImg from '../public/assets/contact.jpg';
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 import { AiOutlineMail } from 'react-icons/ai';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
+import Link from 'next/link';
+import { HiOutlineChevronDoubleUp } from 'react-icons/hi';
 
 
 
@@ -61,12 +63,44 @@ const Contact = () => {
                     className="border-2 rounded-lg p-3 flex border-gray-300"
                     type="text" 
                     />
-
+                </div>
+                <div className='flex flex-col'>
+                    <label className='uppercase text-sm py-2'>Phone Number</label>
+                    <input 
+                    className="border-2 rounded-lg p-3 flex border-gray-300"
+                    type="text" 
+                    />
                 </div>
             </div>
+            <div className='flex flex-col py-2'>
+            <label className='uppercase text-sm py-2'>Email</label>
+            <input 
+                    className="border-2 rounded-lg p-3 flex border-gray-300"
+                    type="email" 
+                    />
+            </div>
+            <div className='flex flex-col py-2'>
+            <label className='uppercase text-sm py-2'>Subject</label>
+            <input 
+                    className="border-2 rounded-lg p-3 flex border-gray-300"
+                    type="text" 
+                    />
+            </div>
+            <div className='flex flex-col py-2'>
+            <label className='uppercase text-sm py-2'>Message</label>
+           <textarea className='border-2 rounded-lg p-3 border-gray-300' rows="10"></textarea>
+            </div>
+            <button className='w-full p-4 text-gray-100 mt-4'>Send Message</button>
         </form>
             </div>
         </div>
+            </div>
+            <div>
+                <Link href="/">
+                    <div>
+                        <HiOutlineChevronDoubleUp/>
+                    </div>
+                </Link>
             </div>
             </div>
         </div>

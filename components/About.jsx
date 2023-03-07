43c32ffaeb1a62b658a35be163d1a5bea0aa4041
Keith Ma <95742118+imkeithyeet@users.noreply.github.com>
2023-03-06 19:38:35 -0800

    resume

--- components/About.jsx
+++ components/About.jsx
@@ -17,7 +17,7 @@ const About = () => {
                    </p>
                    </p>
                    <p className='py-2 text-gray-600'>I have developed this website using Next.JS and Tailwind CSS.</p>
-                   <a href="https://drive.google.com/file/d/10LaBA7vSU8z2ESbqh0WXOw1vpQxAgtzp/view" target="_blank" rel="noopener noreferrer" className='py-2 text-gray-600 underline '>Check out my Resume</a>
+                   <a href="https://drive.google.com/file/d/1ZOonWvRlMncezDCuOPjTrrF73YIqZ1Vt/view" target="_blank" rel="noopener noreferrer" className='py-2 text-gray-600 underline '>Check out my Resume</a>
 
                 </div>
                 <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
